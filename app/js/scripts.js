dd11b82dade4669070240e5ec796700d4f767d65
WalkableSun <jobschopman@icloud.com>
2024-02-22 10:53:44 +0100

    Added popover close on click

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -2,15 +2,18 @@ let mapClicker = document.querySelector(".map");
 let popOver = document.querySelector(".popover")
 
 let body = document.querySelector("body");
+document.addEventListener("click", handleClicks);
 
 function handleClicks (event){
   console.log(event.target);
-
-
-  if(event.target.matches(".map")){
-    event.preventDefault();
+  if (event.target.matches(".map") || event.target.matches(".closer")) {
     body.classList.toggle("showme");
+    event.preventDefault();
+   } else {
+      body.classList.remove("showme");
+    }
   }
-}
+
+
 
 document.addEventListener("click", handleClicks);
